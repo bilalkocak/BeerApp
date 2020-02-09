c2871a6239377841fb825a3aad2d2fe597f79822
Bilal Koçak <kocakbilal04@gmail.com>
2020-02-09 04:58:38 +0100

    Added safearea

--- src/screens/BeerList.js
+++ src/screens/BeerList.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
 
 import BeerListItem from '../components/BeerListItem';
 
@@ -9,33 +9,44 @@ const styles = StyleSheet.create({
 
 export default function BeerList(props) {
     const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
-        fetch('https://api.punkapi.com/v2/beers?page=9&per_page=10')
+
+        getBeers();
+
+    }, [page]);
+
+    function getBeers() {
+        fetch('https://api.punkapi.com/v2/beers')
             .then((response) => {
                 return response.json();
             })
             .then((beers) => {
                 setData(beers);
             });
-    });
+    }
 
-    const { navigate } = props.navigation;
+
+    const {navigate} = props.navigation;
 
     return (
-        <View>
-            <View style={styles.deneme}>
-                <FlatList
-                    data={data}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => navigate('BeerDetail',{id:item.id})}>
-                            <BeerListItem item={item}/>
-                        </TouchableOpacity>)
-                    }
-                    keyExtractor={(item) => item.id.toString()}
-                />
+        <SafeAreaView>
+            <View>
+                <View style={styles.deneme}>
+                    <FlatList
+                        data={data}
+                        renderItem={({item}) => (
+                            <TouchableOpacity onPress={() => navigate('BeerDetail', {id: item.id})}>
+                                <BeerListItem item={item}/>
+                            </TouchableOpacity>)
+                        }
+                        keyExtractor={(item) => item.id.toString()}
+                    />
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
+
     );
 }
 
