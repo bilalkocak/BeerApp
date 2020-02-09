e1de23549fbeda0b8b0ccd71e9cc511ee21455f3
Bilal Koçak <kocakbilal04@gmail.com>
2020-02-09 05:00:21 +0100

    Updated layout and style

--- src/components/BeerListItem.js
+++ src/components/BeerListItem.js
@@ -1,37 +1,59 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
     itemContainer: {
         flex: 1,
-        marginTop: 5,
+        marginTop: 20,
+        marginRight: 20,
+        marginLeft: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingRight: 10,
+        paddingLeft: 10,
         flexDirection: 'row',
+        borderWidth: 1,
+        borderRadius: 25,
+        backgroundColor: '#F8646C',
+
     },
     img: {
-        height: 180
+        height: 220,
     },
     beerTitle: {
         fontWeight: 'bold',
-        fontSize: 16,
+        fontSize: 18,
+        color: 'white',
     },
     beerDescription: {
         fontSize: 14,
         marginTop: 10,
+        color: 'white',
     },
     infoContainer: {
         flex: 5,
+        justifyContent: 'center',
     },
     imageContainer: {
         flex: 1,
-    }
+    },
+    beerTagline: {
+        fontSize: 15,
+        color: 'white',
+    },
 });
 
 const BeerListItem = (props) => {
-    const { item } = props;
+    const {item} = props;
 
     return (
         <View style={styles.itemContainer}>
+            <View style={styles.infoContainer}>
+                <Text style={styles.beerTitle}>{item.name}</Text>
+                <Text style={styles.beerTagline}>{item.tagline}</Text>
+                <Text style={styles.beerDescription}>{item.description.substring(0, 150) + '...'}</Text>
+            </View>
             <View style={styles.imageContainer}>
                 <Image
                     style={styles.img}
@@ -39,10 +61,6 @@ const BeerListItem = (props) => {
                         uri: item.image_url,
                     }}
                 />
-            </View>
-            <View style={styles.infoContainer}>
-                <Text style={styles.beerTitle}>{item.name}</Text>
-                <Text style={styles.beerDescription}>{item.description}</Text>
             </View>
         </View>
 
